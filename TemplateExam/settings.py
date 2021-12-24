053e231f42821dfd90a75e498fe0e8068f3b6f22
Your Name <masaakigoto0802@gmail.com>
2021-12-24 15:35:26 +0900

    ~106

--- TemplateExam/settings.py
+++ TemplateExam/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TENPLATE_DIR = os.path.join(BASE_DIR, 'templates')
@@ -121,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
